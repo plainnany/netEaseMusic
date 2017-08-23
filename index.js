c6a107b8cf1194ddd2f9113c8bc7c5f21ad7dda3
plainnany <plainnany@gmail.com>
2017-08-23 09:47:29 +0800

    add tab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(function(){
-
+    $('.topbar-tab>li').on('click',function(){
+        var index = $(this).index()
+        $(this).addClass('active')
+        $(this).siblings().removeClass('active')
+        $('.tab-content').hide()
+        $('.tab-content').eq(index).show()
+        
+    })
 
 
     
