c3b5174459d6153573d41c81ab579f787cb1838d
plainnany <plainnany@gmail.com>
2017-08-25 22:13:45 +0800

    add music data

---
+++ vendors/av-data.js
@@ -0,0 +1,31 @@
+var APP_ID = 'MTtc1w5rvxtBQtszNzzGxaBi-gzGzoHsz'
+var APP_KEY = 'X3lLWjfgi4fJxMNhhYoR4Ewc'
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+})
+var songObject = AV.Object.extend('Song')
+var songs = new songObject()
+// songs.save({
+//   name: '龙卷风',
+//   singer: '邓紫棋',
+//   url: '//ov8ky0xr6.bkt.clouddn.com/%E9%BE%99%E5%8D%B7%E9%A3%8E.mp3'
+
+// }).then(function(object) {
+//   alert('保存成功')
+// })
+var query = new AV.Query('Song');
+query.find().then(function (results) {
+  // 如果这样写，第二个条件将覆盖第一个条件，查询只会返回 priority = 1 的结果
+    console.log(results)
+    var html = ''
+    for(var i=0;i<results.length;i++){
+        html += '<a href='+ results[i].attributes.url +' class="music-list border">\
+        <h3>'+ results[i].attributes.name +'<small></small></h3>\
+        <p><i class="icon-sq"></i>'+ results[i].attributes.singer +'</p>\
+        <span><i class="icon-play"></i></span></a>'
+    }
+    $('.lastest-list').append(html)
+}, function (error) {
+})
